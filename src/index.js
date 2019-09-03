04fa84356474d9b211cd87692a89ab3f15183d66
Christopher Klapp <christopher@klapp.name>
2019-09-03 17:17:57 -0400

    added src/index.js for npm

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export {default as HelloName} from './components/hello-name';
+export {default as HelloNameListItem} from './components/hello-name-list-item';
+export {default as HelloNames} from './components/hello-names';
+export {default as HelloNamesList} from './components/hello-names-list';
